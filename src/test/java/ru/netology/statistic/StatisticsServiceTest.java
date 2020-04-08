9cf5354480dae8fd1533690ffd1788f3f4720650
Ekaterina Rymareva <rymareva_ea@mail.ru>
2020-04-08 16:42:50 +0500

    change README.md

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -13,10 +13,10 @@ public class StatisticsServiceTest {
        }
     @Test
     void findMaxE2() {
-        StatisticsService service = new StatisticsService();
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 18, 6, 11, 11, 10};
-        long expected = 18;
-        long actual = service.findMax(incomesInBillions);
-        assertEquals(expected, actual);
+       StatisticsService service = new StatisticsService();
+       long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 18, 6, 11, 11, 10};
+       long expected = 18;
+      long actual = service.findMax(incomesInBillions);
+       assertEquals(expected, actual);
     }
 }
