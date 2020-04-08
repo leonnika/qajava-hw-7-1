a146b4e193e8e462c267689566d6f49d38ec57f7
Ekaterina Rymareva <rymareva_ea@mail.ru>
2020-04-08 12:34:55 +0500

    added JaCoCo

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -6,8 +6,8 @@ public class StatisticsServiceTest {
        @Test
     void findMax() {
         StatisticsService service = new StatisticsService();
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 18, 6, 11, 11, 12};
+        long expected = 18;
         long actual = service.findMax(incomesInBillions);
         assertEquals(expected, actual);
     }
