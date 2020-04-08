d5ff488ed854e4c5a1cc481dbda45ae9ca290745
Ekaterina Rymareva <rymareva_ea@mail.ru>
2020-04-08 16:00:53 +0500

    mvn -B -e verify test with INSTRUCTION=100% with tests findMaxEx1, findMaxEx2

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -4,12 +4,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 public class StatisticsServiceTest {
        @Test
-    void findMax() {
+    void findMaxEx1() {
         StatisticsService service = new StatisticsService();
            long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 10};
            long expected = 12;
            long actual = service.findMax(incomesInBillions);
            assertEquals(expected, actual);
        }
-
+    @Test
+    void findMaxE2() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 18, 6, 11, 11, 10};
+        long expected = 18;
+        long actual = service.findMax(incomesInBillions);
+        assertEquals(expected, actual);
+    }
 }
